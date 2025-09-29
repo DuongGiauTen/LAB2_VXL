b334669f767733e952c63e4c28df912afdeef71d
gitTest <duongtranminh253@gmail.com>
2025-09-29 16:33:04 +0700

    add lab6

--- SOURCECODE/Core/Inc/timer.h
+++ SOURCECODE/Core/Inc/timer.h
@@ -9,15 +9,26 @@
 #define INC_TIMER_H_
 
 #define TICK 10
+#define TIMER_CYCLE 10
+
 int count;
 int flag;
 int count2;
 int flag2;
 
+//var of lab2_ex6
+int timer0_counter;
+int timer0_flag;
+
+
+
 void setTimer1(int duration);
 void timer1Run(void);
 void setTimer2(int duration);
 void timer2Run(void);
+// LAB2_ex6 func
+void setTimer0(int duration);
+void timer_run();
 
 
 #endif /* INC_TIMER_H_ */
--- SOURCECODE/Core/Src/timer.c
+++ SOURCECODE/Core/Src/timer.c
@@ -34,3 +34,18 @@ void timer2Run(){
 		}
 	}
 }
+
+
+void setTimer0(int duration){
+	timer0_counter = duration / TIMER_CYCLE;
+	timer0_flag = 0;
+}
+
+void timer_run(){
+	if(timer0_counter > 0){
+		timer0_counter--;
+		if(timer0_counter == 0){
+			timer0_flag = 1;
+		}
+	}
+}

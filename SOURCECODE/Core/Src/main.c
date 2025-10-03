f66005647c1d815beb48bd648af132adf8b4fc45
gitTest <duongtranminh253@gmail.com>
2025-10-03 07:26:23 +0700

    add

--- SOURCECODE/Core/Src/main.c
+++ SOURCECODE/Core/Src/main.c
@@ -103,7 +103,7 @@ int main(void)
   /* USER CODE BEGIN 2 */
 //  setTimer1(250);
 //  setTimer2(250);
-  setTimer0(100);
+  setTimer0(50);
   HAL_TIM_Base_Start_IT(&htim2);
   //int counter = 0;
   //int counterClk = 0;
